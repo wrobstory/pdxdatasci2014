f85ac6c77e3d3fa09ad902f8adcd164123728148
Rob Story <rob@simple.com>
2014-11-10 22:29:21 -0800

    ENH: First materials

---
+++ pipinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+virtualenv .env &&
+source .env/bin/activate &&
+brew install libevent &&
+brew install libyaml &&
+pip install cython &&
+pip install numpy &&
+pip install scipy &&
+pip install pandas &&
+pip install matplotlib &&
+pip install seaborn &&
+pip install bokeh
